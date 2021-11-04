b6032d9de6b7f5764dec827854ee71518e3bbe9d
ChaimaElharti <91948783+ChaimaElharti@users.noreply.github.com>
2021-11-04 16:49:32 +0100

    Variation de la température
    Questions 1 et 2

--- simulation.c
+++ simulation.c
@@ -32,6 +32,23 @@ static real G(real T, real co2)
     return 3.35071874e-03 * T + 3.20986702e-05 * co2 - 0.561593690144655;
 }
 
+//VARIATION DE LA TEMPÉRATURE//
+
+//Q.1
+static real real_P_in() {
+    return (1-ALBEDO)*(S0 / 4);
+}
+
+//Q.1
+real real_P_out(real t, real T){
+    return (1-G(T, CO20))*SIGMA * T * T * T * T;
+}
+
+//Q.2
+real real_F(real t, real T) {
+    return (real_P_in()-real_P_out(t, T))*(1/100);
+}
+
 int main(int argc, char **argv)
 {
     printf("Simple climate simulation\n");
